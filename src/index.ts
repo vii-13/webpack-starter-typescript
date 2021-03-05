446932f729d8e47a637988905be9e71ca3068e0b
Esterlyn <diazesterlin62@gmail.com>
2021-03-05 11:24:45 -0400

    Insertando datos en Firestore

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-import './firebase/config';
+import db from './firebase/config';
 
+const usuario = {
+    nombre : 'Miguel',
+    activo : false,
+    nacimiento : 0,
+}
+
+// insertando datos
+
+// db.collection('usuarios')
+// .add(usuario)
