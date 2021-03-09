d71453ee58cc8493609d3bb5bff788ff5667ca5c
Esterlyn <diazesterlin62@gmail.com>
2021-03-08 21:44:31 -0400

    Utilizando Paginacion y Limit

--- src/index.ts
+++ src/index.ts
@@ -68,3 +68,51 @@ const usuariosRef = db.collection('usuarios');
 //       .orderBy('salario')
 //       .get().then(retornaDocumentos)
 
+
+// paginacion-----
+// usuariosRef.limit(3)
+//      .get().then(retornaDocumentos);
+
+// ----------
+// const btnNext = document.createElement('button');
+// btnNext.innerText = 'new page';
+// document.body.append(btnNext)
+
+// let firstDocument:any =null;
+// let lastDocument:any =null;
+// btnNext.addEventListener('click', () => {
+    
+//     const query = usuariosRef
+//                     .orderBy('nombre')
+//                     .startAfter(lastDocument)
+
+//     query.limit(2).get().then(snap => {
+
+//         firstDocument = snap.docs[ 0 ] || null;
+//         lastDocument = snap.docs[ snap.docs.length - 1] || null;
+//         retornaDocumentos(snap);
+//     })                    
+// });
+
+// btnNext.click();
+
+// // btn previous------
+// const btnPrev = document.createElement('button');
+// btnPrev.innerText = 'Previous page';
+// document.body.append(btnPrev)
+
+
+// btnPrev.addEventListener('click', () => {
+    
+//     const query = usuariosRef
+//                     .orderBy('nombre')
+//                     .endBefore(firstDocument)
+
+//     query.limit(2).get().then(snap => {
+
+//         firstDocument = snap.docs[ 0 ] || null;
+//         lastDocument = snap.docs[ snap.docs.length - 1] || null;
+//         retornaDocumentos(snap);
+//     })                    
+// });
+
